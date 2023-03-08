39f53e36095a011225da2c6d8cdf4ea85e0ac535
Ahmed Maher <62140280+AhmedMaherSayed@users.noreply.github.com>
2023-03-08 07:00:31 +0200

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,6 @@
+let container = document.querySelector('.container');
+let signupBtn = document.querySelector('.green-bg button');
+
+signupBtn.addEventListener('click', ()=>{
+    container.classList.toggle('change');
+});
